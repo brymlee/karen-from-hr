adc539141c6e7aff6cf85b79033d701faaf21935
brymlee <brymlee1911@gmail.com>
2021-01-06 16:47:02 -0600

    Changing to cron execution style

--- index.ts
+++ index.ts
@@ -3,25 +3,37 @@ import { Client, TextChannel } from 'discord.js';
 import axios from 'axios';
 
 const client = new Client();
-client.on('message', message => {
-  console.log(`Connected as ${client.user.tag}`);
-  console.log(message);
-  const textChannel = message.channel as TextChannel;
-  if(message.content.includes('What is the exchange rate?')){
-    axios.request({ 
-      method: 'get',
-      url: `https://v1.nocodeapi.com/brymlee/cx/${process.env.NO_CODE_API_SECRET_TOKEN}/rates`,
-      params: { 'target': 'JPY', 'source': 'USD' },
-    }).then(res => {
-      const { source, date, rates } = res.data as Record<'source' | 'date', string> & Record<'rates', Record<'JPY', number>>;
-      const { JPY } = rates;
-      const result = `As of ${date}, for every 1 ${source} you will get ${JPY} yen.`;
-      console.log(result);
-      message.channel.messages.channel.send(result)
-    }, err => {
-      console.log(`Error getting exchange rate ${err}`);
-    });
-  }
-});
 
 client.login(process.env.KAREN_FROM_HR_SECRET_TOKEN);
+
+client.on('ready', async () => {
+  console.log(`Connected as ${client.user.tag}`);
+  await client.channels.fetch('795699539209879612').then(
+    async channel => {
+      const textChannel = channel as TextChannel;
+      const result: string | undefined = await axios.request({ 
+        method: 'get',
+        url: `https://v1.nocodeapi.com/brymlee/cx/${process.env.NO_CODE_API_SECRET_TOKEN}/rates`,
+        params: { 'target': 'JPY', 'source': 'USD' },
+      }).then(res => {
+        const { source, date, rates } = res.data as Record<'source' | 'date', string> & Record<'rates', Record<'JPY', number>>;
+        const { JPY } = rates;
+        const result = `As of ${date}, for every 1 ${source} you will get ${JPY} yen.`;
+        return result;
+      }, err => {
+        const message = `Error getting exchange rate ${err}`;
+        console.log(message);
+        return undefined;
+      });
+      if(result){
+        await textChannel.messages.channel.send(result);
+        return;
+      } 
+      return;
+    },
+    err => {
+      console.log(`Error with karen ${err}`);
+    },
+  );
+  client.destroy();
+});
