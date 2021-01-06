ca0f913fede6c6de7e482ba9e67408fbdf690d39
brymlee <brymlee1911@gmail.com>
2021-01-05 19:11:18 -0600

    Exchange rate

--- index.ts
+++ index.ts
@@ -1,17 +1,27 @@
 require('dotenv').config();
-import { Client } from 'discord.js';
+import { Client, TextChannel } from 'discord.js';
+import axios from 'axios';
 
 const client = new Client();
-client.on('ready', () => {
+client.on('message', message => {
   console.log(`Connected as ${client.user.tag}`);
-  client.channels.fetch('795753554127093800').then(
-    channel => {
-      console.log(channel);
-    },
-    err => {
-      console.log(`Error fetching discord channel ${err}`);
-    }
-  );
+  console.log(message);
+  const textChannel = message.channel as TextChannel;
+  if(message.content.includes('What is the exchange rate?')){
+    axios.request({ 
+      method: 'get',
+      url: `https://v1.nocodeapi.com/brymlee/cx/${process.env.NO_CODE_API_SECRET_TOKEN}/rates`,
+      params: { 'target': 'JPY', 'source': 'USD' },
+    }).then(res => {
+      const { source, date, rates } = res.data as Record<'source' | 'date', string> & Record<'rates', Record<'JPY', number>>;
+      const { JPY } = rates;
+      const result = `As of ${date}, for every 1 ${source} you will get ${JPY} yen.`;
+      console.log(result);
+      message.channel.messages.channel.send(result)
+    }, err => {
+      console.log(`Error getting exchange rate ${err}`);
+    });
+  }
 });
 
 client.login(process.env.KAREN_FROM_HR_SECRET_TOKEN);
