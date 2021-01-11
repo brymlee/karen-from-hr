d773fd28598bca19f10d9fd00618977867dee81f
brymlee <brymlee1911@gmail.com>
2021-01-11 14:35:15 -0600

    Fixing compilation

--- index.ts
+++ index.ts
@@ -10,11 +10,13 @@ client.on('message', message => {
   if(message.channel instanceof TextChannel && message.mentions.users.some(user => user.username.includes('Karen from hr'))){
     const textChannel = message.channel.messages.channel as TextChannel;
     messageResponse(message.content).then(response => {
-      textChannel.send(`@${message.author.username} - ${response}`).then(_nothing => {
-        textChannel.stopTyping(true);
-      }, err => {
-        console.log(`Error sending message ${err}`);
-      });
+      textChannel.send(`@${message.author.username} - ${response}`).then(
+        _nothing => {},
+        err => {
+          console.log(`Error sending message ${err}`);
+        },
+      );
+      return;
     }, err => {
       console.log(`Error getting response to message ${err}`);
     });
@@ -23,7 +25,7 @@ client.on('message', message => {
 
 async function messageResponse(it: string): Promise<string>{
   if(it.toLowerCase().includes('exchange rate') && it.toLowerCase().includes('?')){
-    return exchangeRate();
+    return exchangeRate(true).then(it => it as string);
   }
   return new Promise((resolve, _reject) => resolve('What do you need?'));
 }
